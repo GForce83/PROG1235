422a0df43295fb29903d3cd7b6485d41c585b68a
micheltremblay <michel.tremblay@ccnb.ca>
2019-06-04 09:47:22 -0300

    PROG 1235 Changer la couleur arrière-plan

---
+++ PROG1235Test/PROG1235Test/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PROG1235Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Changer la couleur arrière-plan à une couleur de votre choix.");
+            this.BackColor = Color.Yellow; // Changer cette couleur;
+        }
+    }
+}
